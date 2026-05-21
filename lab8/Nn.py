d89753e598e6892898c15129a8b1e1567426371f
emildavydov007 <davydov.e945@gmail.com>
2026-05-21 15:03:45 +0500

    Update Nn.py

--- lab8/Nn.py
+++ lab8/Nn.py
@@ -1,4 +1,127 @@
-if top_text:
+if top_text:from PIL import Image, ImageDraw, ImageFont
+
+
+class MemeGenerator:
+
+    def __init__(self, image_path):
+        self.image = Image.open(image_path)
+
+    def add_text(self, top_text="", bottom_text=""):
+
+        image = self.image.copy()
+
+        draw = ImageDraw.Draw(image)
+
+        width, height = image.size
+
+        try:
+            # Нормальный размер шрифта
+            font_size = width // 10
+
+            font = ImageFont.truetype(
+                "arial.ttf",
+                font_size
+            )
+
+        except:
+            font = ImageFont.load_default()
+
+        # Функция переноса строк
+        def wrap(text):
+
+            words = text.upper().split()
+
+            lines = []
+
+            current = ""
+
+            for word in words:
+
+                test = (
+                    current + " " + word
+                    if current
+                    else word
+                )
+
+                bbox = draw.textbbox(
+                    (0, 0),
+                    test,
+                    font=font
+                )
+
+                text_width = bbox[2]
+
+                # Ограничение по ширине
+                if text_width < width - 80:
+                    current = test
+
+                else:
+                    lines.append(current)
+                    current = word
+
+            if current:
+                lines.append(current)
+
+            return lines
+
+        # Рисование текста
+        def draw_lines(lines, start_y):
+
+            line_height = font_size + 10
+
+            for line in lines:
+
+                bbox = draw.textbbox(
+                    (0, 0),
+                    line,
+                    font=font
+                )
+
+                text_width = bbox[2]
+
+                x = (
+                    width - text_width
+                ) // 2
+
+                draw.text(
+                    (x, start_y),
+                    line,
+                    font=font,
+                    fill="white",
+                    stroke_width=5,
+                    stroke_fill="black"
+                )
+
+                start_y += line_height
+
+        # Верхний текст
+        if top_text:
+
+            lines = wrap(top_text)
+
+            draw_lines(
+                lines,
+                30
+            )
+
+        # Нижний текст
+        if bottom_text:
+
+            lines = wrap(bottom_text)
+
+            start_y = (
+                height
+                - len(lines)
+                * (font_size + 10)
+                - 30
+            )
+
+            draw_lines(
+                lines,
+                start_y
+            )
+
+        return image
 
             self.draw_centered_text(
 
